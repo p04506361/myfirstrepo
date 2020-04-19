229dc39a5c5a641bfe35c27921a10e9537ad4c28
Paresh <Paresh@gmail.com>
2020-04-19 05:38:54 +0000

    multiply change

--- calc.py
+++ calc.py
@@ -1,12 +1,9 @@
 def add(x,y):
 	return x + y
 def subtract(x,y):
-<<<<<<< HEAD
-   
 	return x - y
 def multiply(x,y):
 	return x * Y
->>>>>>> c8aa3677a28bbd52c24e897e3ae3f0f6ffa0be97
 def divide(x,y):
 	return x / y
 def square(x,y):
